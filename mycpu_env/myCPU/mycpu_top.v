0a489987a992965eabf865195b403ec6a209ea3b
zenghz22 <zenghz22@mails.tsinghua.edu.cn>
2024-07-14 23:03:00 +0800

    更新了IF和ID的流水线

--- mycpu_env/myCPU/mycpu_top.v
+++ mycpu_env/myCPU/mycpu_top.v
@@ -1,26 +1,29 @@
+//if_id_inst
 module mycpu_top(
-    input  wire        clk,
-    input  wire        resetn,
-    // inst sram interface
-    output wire        inst_sram_we,
-    output wire [31:0] inst_sram_addr,
-    output wire [31:0] inst_sram_wdata,
-    input  wire [31:0] inst_sram_rdata,
-    // data sram interface
-    output wire        data_sram_we,
-    output wire [31:0] data_sram_addr,
-    output wire [31:0] data_sram_wdata,
-    input  wire [31:0] data_sram_rdata,
-    // trace debug interface
-    output wire [31:0] debug_wb_pc,
-    output wire [ 3:0] debug_wb_rf_we,
-    output wire [ 4:0] debug_wb_rf_wnum,
-    output wire [31:0] debug_wb_rf_wdata
-);
-reg         reset;
+  input  wire        clk,
+  input  wire        resetn,
+  // inst sram interface
+  output wire        inst_sram_en
+  output wire [3:0]  inst_sram_we,
+  output wire [31:0] inst_sram_addr,
+  output wire [31:0] inst_sram_wdata,
+  input  wire [31:0] inst_sram_rdata,
+  // data sram interface
+  output wire        data_sram_en 
+  output wire [3:0]  data_sram_we,
+  output wire [31:0] data_sram_addr,
+  output wire [31:0] data_sram_wdata,
+  input  wire [31:0] data_sram_rdata,
+  // trace debug interface
+  output wire [31:0] debug_wb_pc,
+  output wire [ 3:0] debug_wb_rf_we,
+  output wire [ 4:0] debug_wb_rf_wnum,
+  output wire [31:0] debug_wb_rf_wdata
+)；
+reg reset；
 always @(posedge clk) reset <= ~resetn;
 
-reg         valid;
+reg valid
 always @(posedge clk) begin
     if (reset) begin
         valid <= 1'b0;
@@ -30,28 +33,53 @@ always @(posedge clk) begin
     end
 end
 
+
+// IF Stage
+reg  [31:0] if_pc;
 wire [31:0] seq_pc;
 wire [31:0] nextpc;
-wire        br_taken;
-wire [31:0] br_target;
-wire [31:0] inst;
-reg  [31:0] pc;
+assign seq_pc       = pc + 3'h4;
+assign nextpc       = br_taken ? br_target : seq_pc;
 
-wire [11:0] alu_op;
-wire        load_op;
-wire        src1_is_pc;
-wire        src2_is_imm;
-wire        res_from_mem;
-wire        dst_is_r1;
-wire        gr_we;
-wire        mem_we;
-wire        src_reg_is_rd;
-wire [4: 0] dest;
-wire [31:0] rj_value;
-wire [31:0] rkd_value;
-wire [31:0] imm;
-wire [31:0] br_offs;
-wire [31:0] jirl_offs;
+always @(posedge clk) begin
+    if (reset)
+        if_pc <= 32'h1bfffffc;
+    else
+        if_pc <= nextpc;
+end
+
+assign inst_sram_we = 1'b0;
+assign inst_sram_addr = if_pc;
+assign inst_sram_wdata = 32'b0;
+
+reg [31:0] if_id_pc;
+reg [31:0] if_id_inst;
+always @(posedge clk) begin
+    if (reset) begin
+        if_id_pc <= 0;
+        if_id_inst <= 0;
+    end else begin
+        if_id_pc <= if_pc;
+        if_id_inst <= inst_sram_rdata;
+    end
+end
+
+
+//ID Stage
+reg  [31:0] id_pc;
+wire [11:0] i12;
+wire [19:0] i20;
+wire [15:0] i16;
+wire [25:0] i26;
+
+always @(posedge clk) begin
+      id_pc <= if_id_pc;
+      i12 <= if_id_inst[21:10];
+      i20 <= if_id_inst[24: 5];
+      i16 <= if_id_inst[25:10];
+      i26 <= {if_id_inst[ 9: 0], if_id_inst[25:10]};
+end
+
 
 wire [ 5:0] op_31_26;
 wire [ 3:0] op_25_22;
@@ -60,15 +88,19 @@ wire [ 4:0] op_19_15;
 wire [ 4:0] rd;
 wire [ 4:0] rj;
 wire [ 4:0] rk;
-wire [11:0] i12;
-wire [19:0] i20;
-wire [15:0] i16;
-wire [25:0] i26;
 
 wire [63:0] op_31_26_d;
 wire [15:0] op_25_22_d;
 wire [ 3:0] op_21_20_d;
 wire [31:0] op_19_15_d;
+
+wire [ 4:0] rf_raddr1;
+wire [31:0] rf_rdata1;
+wire [ 4:0] rf_raddr2;
+wire [31:0] rf_rdata2;
+wire        rf_we   ;
+wire [ 4:0] rf_waddr;
+wire [31:0] rf_wdata;
 
 wire        inst_add_w;
 wire        inst_sub_w;
@@ -91,57 +123,14 @@ wire        inst_beq;
 wire        inst_bne;
 wire        inst_lu12i_w;
 
-wire        need_ui5;
-wire        need_si12;
-wire        need_si16;
-wire        need_si20;
-wire        need_si26;
-wire        src2_is_4;
+assign op_31_26  = if_id_inst[31:26];
+assign op_25_22  = if_id_inst[25:22];
+assign op_21_20  = if_id_inst[21:20];
+assign op_19_15  = if_id_inst[19:15];
 
-wire [ 4:0] rf_raddr1;
-wire [31:0] rf_rdata1;
-wire [ 4:0] rf_raddr2;
-wire [31:0] rf_rdata2;
-wire        rf_we   ;
-wire [ 4:0] rf_waddr;
-wire [31:0] rf_wdata;
-
-wire [31:0] alu_src1   ;
-wire [31:0] alu_src2   ;
-wire [31:0] alu_result ;
-wire [31:0] final_result;
-wire [31:0] mem_result;
-
-assign seq_pc       = pc + 3'h4;
-assign nextpc       = br_taken ? br_target : seq_pc;
-
-always @(posedge clk) begin
-    if (reset) begin
-        pc <= 32'h1bfffffc;     //trick: to make nextpc be 0x1c000000 during reset 
-    end
-    else begin
-        pc <= nextpc;
-    end
-end
-
-assign inst_sram_we    = 1'b0;
-assign inst_sram_addr  = pc;
-assign inst_sram_wdata = 32'b0;
-assign inst            = inst_sram_rdata;
-
-assign op_31_26  = inst[31:26];
-assign op_25_22  = inst[25:22];
-assign op_21_20  = inst[21:20];
-assign op_19_15  = inst[19:15];
-
-assign rd   = inst[ 4: 0];
-assign rj   = inst[ 9: 5];
-assign rk   = inst[14:10];
-
-assign i12  = inst[21:10];
-assign i20  = inst[24: 5];
-assign i16  = inst[25:10];
-assign i26  = {inst[ 9: 0], inst[25:10]};
+assign rd   = if_id_inst[ 4: 0];
+assign rj   = if_id_inst[ 9: 5];
+assign rk   = if_id_inst[14:10];
 
 decoder_6_64 u_dec0(.in(op_31_26 ), .out(op_31_26_d ));
 decoder_4_16 u_dec1(.in(op_25_22 ), .out(op_25_22_d ));
@@ -169,58 +158,6 @@ assign inst_beq    = op_31_26_d[6'h16];
 assign inst_bne    = op_31_26_d[6'h17];
 assign inst_lu12i_w= op_31_26_d[6'h05] & ~inst[25];
 
-assign alu_op[ 0] = inst_add_w | inst_addi_w | inst_ld_w | inst_st_w
-                    | inst_jirl | inst_bl;
-assign alu_op[ 1] = inst_sub_w;
-assign alu_op[ 2] = inst_slt;
-assign alu_op[ 3] = inst_sltu;
-assign alu_op[ 4] = inst_and;
-assign alu_op[ 5] = inst_nor;
-assign alu_op[ 6] = inst_or;
-assign alu_op[ 7] = inst_xor;
-assign alu_op[ 8] = inst_slli_w;
-assign alu_op[ 9] = inst_srli_w;
-assign alu_op[10] = inst_srai_w;
-assign alu_op[11] = inst_lu12i_w;
-
-assign need_ui5   =  inst_slli_w | inst_srli_w | inst_srai_w;
-assign need_si12  =  inst_addi_w | inst_ld_w | inst_st_w;
-assign need_si16  =  inst_jirl | inst_beq | inst_bne;
-assign need_si20  =  inst_lu12i_w;
-assign need_si26  =  inst_b | inst_bl;
-assign src2_is_4  =  inst_jirl | inst_bl;
-
-assign imm = src2_is_4 ? 32'h4                      :
-             need_si20 ? {i20[19:0], 12'b0}         :
-/*need_ui5 || need_si12*/{{20{i12[11]}}, i12[11:0]} ;
-
-assign br_offs = need_si26 ? {{ 4{i26[25]}}, i26[25:0], 2'b0} :
-                             {{14{i16[15]}}, i16[15:0], 2'b0} ;
-
-assign jirl_offs = {{14{i16[15]}}, i16[15:0], 2'b0};
-
-assign src_reg_is_rd = inst_beq | inst_bne | inst_st_w;
-
-assign src1_is_pc    = inst_jirl | inst_bl;
-
-assign src2_is_imm   = inst_slli_w |
-                       inst_srli_w |
-                       inst_srai_w |
-                       inst_addi_w |
-                       inst_ld_w   |
-                       inst_st_w   |
-                       inst_lu12i_w|
-                       inst_jirl   |
-                       inst_bl     ;
-
-assign res_from_mem  = inst_ld_w;
-assign dst_is_r1     = inst_bl;
-assign gr_we         = ~inst_st_w & ~inst_beq & ~inst_bne & ~inst_b;
-assign mem_we        = inst_st_w;
-assign dest          = dst_is_r1 ? 5'd1 : rd;
-
-assign rf_raddr1 = rj;
-assign rf_raddr2 = src_reg_is_rd ? rd :rk;
 regfile u_regfile(
     .clk    (clk      ),
     .raddr1 (rf_raddr1),
@@ -232,44 +169,34 @@ regfile u_regfile(
     .wdata  (rf_wdata )
     );
 
-assign rj_value  = rf_rdata1;
-assign rkd_value = rf_rdata2;
 
-assign rj_eq_rd = (rj_value == rkd_value);
-assign br_taken = (   inst_beq  &&  rj_eq_rd
-                   || inst_bne  && !rj_eq_rd
-                   || inst_jirl
-                   || inst_bl
-                   || inst_b
-                  ) && valid;
-assign br_target = (inst_beq || inst_bne || inst_bl || inst_b) ? (pc + br_offs) :
-                                                   /*inst_jirl*/ (rj_value + jirl_offs);
 
-assign alu_src1 = src1_is_pc  ? pc[31:0] : rj_value;
-assign alu_src2 = src2_is_imm ? imm : rkd_value;
 
-alu u_alu(
-    .alu_op     (alu_op    ),
-    .alu_src1   (alu_src1  ),
-    .alu_src2   (alu_src2  ),
-    .alu_result (alu_result)
-    );
 
-assign data_sram_we    = mem_we && valid;
-assign data_sram_addr  = alu_result;
-assign data_sram_wdata = rkd_value;
 
-assign mem_result   = data_sram_rdata;
-assign final_result = res_from_mem ? mem_result : alu_result;
 
-assign rf_we    = gr_we && valid;
-assign rf_waddr = dest;
-assign rf_wdata = final_result;
 
-// debug info generate
-assign debug_wb_pc       = pc;
-assign debug_wb_rf_we   = {4{rf_we}};   //assign debug_wb_rf_wen   = {4{rf_we}};
-assign debug_wb_rf_wnum  = dest;
-assign debug_wb_rf_wdata = final_result;
+//级间寄存器存储(参考)
+//assign op_31_26  = inst[31:26];
+//assign rd   = inst[ 4: 0];
 
-endmodule
+// IF/ID Register
+reg [31:0] if_id_pc;
+reg [31:0] if_id_inst；
+
+// ID/EX Register
+reg  [31:0] id_ex_pc;
+reg [31:0] id_ex_rj_value;
+reg [31:0] id_ex_rkd_value;
+reg [31:0] id_ex_inst;
+
+// EX/MEM Register
+reg [31:0] ex_mem_pc;
+reg [31:0] ex_mem_rj_value;
+reg [31:0] ex_mem_rkd_value;
+reg [31:0] ex_mem_inst;
+
+// MEM/WB Register
+reg [31:0] mem_wb_pc;
+reg [31:0] mem_wb_inst;
+
